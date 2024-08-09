63ecb0caf26633f7dae222c6259059120b457d85
rpoojitha67 <rpoojitha2003@gmail.com>
2024-08-09 23:48:14 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,19 @@
+function my(){
+            window.location.href="ordernow.html";
+        }
+function menswear(){
+    window.location.href="/men html files/mensfile.html";
+}
+
+function home(){
+    window.location.href="shop.html";
+}
+function womenwear(){
+    window.location.href="/women html files/womenfile.html";
+}
+function acc(){
+    window.location.href="/acc html files/accfile.html";
+}
+function home(){
+    window.location.href="shop.html";
+}
